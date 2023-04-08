c3286ef30c0dfe31fb8b26aa0c0493a2a077b87c
VenkatNarayanan95 <124430836+VenkatNarayanan95@users.noreply.github.com>
2023-04-08 15:04:58 +0530

    Adding Maven logger project

---
+++ src/main/java/com/atlas/AtlasJenkinsDemoApplication.java
@@ -0,0 +1,22 @@
+package com.atlas;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.*;
+@SpringBootApplication
+public class AtlasJenkinsDemoApplication implements CommandLineRunner {
+	public static final Logger Logger= LoggerFactory.getLogger(AtlasJenkinsDemoApplication.class);
+
+	public static void main(String[] args) {
+		Logger.info("its a CI pipeline job");
+		SpringApplication.run(AtlasJenkinsDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Logger.info("second log");
+		
+	}
+
+}
